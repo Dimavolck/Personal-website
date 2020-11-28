962f328a0775c4cf34539f1ad45ae6714adbcbae
Dimavolck <71402257+Dimavolck@users.noreply.github.com>
2020-11-27 21:28:46 -0800

    JS update
    first function

--- JavaS/script.js
+++ JavaS/script.js
@@ -1,32 +1,17 @@
 
-let courseList = [
-    {code: 'ACIT 1620', name: 'Web Fundamental Technologies',  num: '1620'},
-    {code: 'ACIT 1420', name: 'Intro to Systems Admin',  num: '1420'},
-    {code: 'ACIT 1630', name: 'Database Systems',  num: '1630'}
-]
+function createCourseArray() {
+    var courseList = new Array();
+    var courseElements = document.getElementsByClassName()
+    var i;
+    for (i = 0; courseElements.length; i++) {
+        courseList.push({code:courseElements[i].text, + "date:'Fall 2020'"})
+    }
+    return courseList;
+}
+
+
+
 
 var userNum = prompt('Please enter a 4-digit number');
 
-while (userNum.length !== 4){
-    if (userNum.length == 4){
-        userNum = prompt('Please enter a valid 4-digit number');
-    }
-}
 
-for (var i = 0; i < courseList.length; i++){
-    coursenumber = courseList[i].num;
-    if (userNum == coursenumber){
-        console.log(`Yes I am taking the course: ${courseList[i].code}-${courseList[i].name}`);
-        break;
-    }
-    else {
-        courseList.push(
-            {
-                code: userNum, name: null, num: userNum
-            }
-        )
-        console.log(courseList);
-        console.log('Course has been successfully added.');
-        break;
-    }
-}
